bd43fadf1a29b04a0e2a3bc40590e30e34b27280
johnbangla <johnbangla@gmail.com>
2022-04-07 13:11:07 +0600

    first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  env: {
+      "BASE_URL": "http://localhost:3000",
+      "MONGODB_URL_local": "mongodb://localhost/jsofteshop",
+      "MONGODB_URL": "mongodb+srv://johnbangla:Dev_1234@cluster0.duyhv.mongodb.net/eshopdb?retryWrites=true&w=majority",
+      "ACCESS_TOKEN_SECRET": "121212",
+      "REFRESH_TOKEN_SECRET": "asasa2323",
+      
+      "CLOUD_URL":"https://api.cloudinary.com/v1_1",
+      "CLOUD_UPDATE_PRESET": "jd2kko9k",
+      "CLOUD_NAME": "ddapxyhwy",
+      "CLOUD_API": "123185621223647",
 
-module.exports = nextConfig
+      "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
+  },
+  images: {
+      domains: ["res.cloudinary.com"],
+  },
+}
